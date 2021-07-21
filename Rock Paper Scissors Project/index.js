1292036070b8abc2baf1818abdd815d966e0793b
ImBilly <56002652+ImBilly01@users.noreply.github.com>
2021-07-21 04:49:04 +0200

    Add files via upload

--- Rock Paper Scissors Project/index.js
+++ Rock Paper Scissors Project/index.js
@@ -4,34 +4,36 @@ let computerPoints = 0;
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const restart = document.getElementById('restart');
 
 function computerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * choices.length);
+
     return choices[random];
 }
 
 function round(user, computer) {
     if (userPoints < 5 && computerPoints < 5) {
         if (user === computer) {
-            document.getElementById('results').innerHTML = 'Tie';
+            document.getElementById('results').innerHTML = 'Results: Tie';
         } else if (user == 'paper' && computer == 'rock') {
-            document.getElementById('results').innerHTML = 'You won!';
+            document.getElementById('results').innerHTML = 'Results: You won!';
             userPoints += 1; 
         } else if (user == 'rock' && computer == 'scissors') {
-            document.getElementById('results').innerHTML = 'You won!';
+            document.getElementById('results').innerHTML = 'Results: You won!';
             userPoints += 1;
         } else if (user == 'scissors' && computer == 'paper') {
-            document.getElementById('results').innerHTML = 'You won!';
+            document.getElementById('results').innerHTML = 'Results: You won!';
             userPoints += 1;
         } else if (user == 'paper' && computer == 'scissors') {
-            document.getElementById('results').innerHTML = 'You lost!';
+            document.getElementById('results').innerHTML = 'Results: You lost!';
             computerPoints += 1;
         } else if (user == 'rock' && computer == 'paper') {
-            document.getElementById('results').innerHTML = 'You lost!';
+            document.getElementById('results').innerHTML = 'Results: You lost!';
             computerPoints += 1;
         } else if (user == 'scissors' && computer == 'rock') {
-            document.getElementById('results').innerHTML = 'You lost!';
+            document.getElementById('results').innerHTML = 'Results: You lost!';
             computerPoints += 1;
         }
         
@@ -64,3 +66,14 @@ paper.addEventListener('click', () => {
 scissors.addEventListener('click', () => {
     round('scissors', computerChoice());
 });
+
+restart.addEventListener('click', () => {
+    userPoints = 0;
+    computerPoints = 0;
+
+    document.getElementById('userScore').innerHTML = '0';
+    document.getElementById('computerScore').innerHTML = '0';
+    document.getElementById('gameResults').innerHTML = 'First to 5 wins.';
+    document.getElementById('results').innerHTML = 'Results:';
+});
+
